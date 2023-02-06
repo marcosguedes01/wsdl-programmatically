188f2ef7d5e30bc56b198200b067a332155e9d21
Marcos Guedes <marcos.guedes@avanade.com>
2023-02-06 08:45:00 -0300

    Initial Files

---
+++ ConsumeWsdlExample/Controllers/HomeController.cs
@@ -0,0 +1,118 @@
+﻿using ConsumeWsdlExample.Xmls;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsumeWsdlExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ConsultarCEP();
+
+            return View();
+        }
+
+        public HttpWebRequest CreateWebRequest()
+        {
+            HttpWebRequest webRequest = (HttpWebRequest) WebRequest.Create(@"https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl");
+
+            webRequest.Headers.Add(@"SOAP:Action");
+            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
+            webRequest.Accept = "text/xml";
+            webRequest.Method = "POST";
+            return webRequest;
+        }
+
+
+        private void ConsultarCEP()
+        {
+            var cep = new Cep
+            {
+                Name = "cep",
+                TextContent = "54280-298"
+            };
+
+            var consultaCep = new ConsultaCep {
+                Name = "consultaCEP",
+                Children = new List<Xmls.XmlElement> { cep }
+            };
+
+            var header = new Header
+            {
+                Name = nameof(Header)
+            };
+            var body = new BodyRequest
+            {
+                Name = "Body",
+                Children = new List<Xmls.XmlElement> { consultaCep }
+            };
+            var envelope = new EnvelopeRequest
+            {
+                Name = "Envelope",
+                Children = new List<Xmls.XmlElement> { header, body }
+            };
+
+            var xmlRequest = new XmlGenerator().GenerateToString(envelope);
+
+
+            HttpWebRequest request = CreateWebRequest();
+            XmlDocument soapEnvelopeXml = new XmlDocument();
+            soapEnvelopeXml.LoadXml(xmlRequest);
+
+
+            using (Stream stream = request.GetRequestStream())
+            {
+                soapEnvelopeXml.Save(stream);
+            }
+
+            using (WebResponse response = request.GetResponse())
+            {
+                using (StreamReader rd = new StreamReader(response.GetResponseStream()))
+                {
+                    string soapResult = rd.ReadToEnd();
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.LoadXml(soapResult);
+
+                    XmlSerializer serializer = new XmlSerializer(typeof(Envelope));
+                    StringReader rdr = new StringReader(soapResult);
+                    Envelope resultingMessage = (Envelope) serializer.Deserialize(rdr);
+                }
+            }
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    [XmlElementPrefix("soapenv", Namespace = "http://schemas.xmlsoap.org/soap/envelope/", SetNamespace = true)]
+    [XmlElementAttr("cli", "http://cliente.bean.master.sigep.bsb.correios.com.br/")]
+    public class EnvelopeRequest : Xmls.XmlElement { }
+
+    [XmlElementPrefix("soapenv", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Header : Xmls.XmlElement { }
+
+    [XmlElementPrefix("soapenv", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class BodyRequest : Xmls.XmlElement { }
+
+    [XmlElementPrefix("cli", Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")]
+    public class ConsultaCep : Xmls.XmlElement { }
+
+    public class Cep : Xmls.XmlElement { }
+}
---
+++ ConsumeWsdlExample/Controllers/XmlClasses.cs
@@ -0,0 +1,54 @@
+﻿//
+
+/* 
+ Licensed under the Apache License, Version 2.0
+
+ http://www.apache.org/licenses/LICENSE-2.0
+ */
+using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+namespace ConsumeWsdlExample.Controllers
+{
+    [XmlRoot(ElementName = "return")]
+    public class Return
+    {
+        [XmlElement(ElementName = "bairro")]
+        public string Bairro { get; set; }
+
+        [XmlElement(ElementName = "cep")]
+        public string Cep { get; set; }
+        [XmlElement(ElementName = "cidade")]
+        public string Cidade { get; set; }
+        [XmlElement(ElementName = "complemento2")]
+        public string Complemento2 { get; set; }
+        [XmlElement(ElementName = "end")]
+        public string End { get; set; }
+        [XmlElement(ElementName = "uf")]
+        public string Uf { get; set; }
+    }
+
+    [XmlRoot(ElementName = "consultaCEPResponse", Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")]
+    public class ConsultaCEPResponse
+    {
+        [XmlElement(ElementName = "return", Namespace = "")]
+        public Return Return { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Body
+    {
+        [XmlElement(ElementName = "consultaCEPResponse", Namespace = "http://cliente.bean.master.sigep.bsb.correios.com.br/")]
+        public ConsultaCEPResponse ConsultaCEPResponse { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Envelope", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+    public class Envelope
+    {
+        [XmlElement(ElementName = "Body", Namespace = "http://schemas.xmlsoap.org/soap/envelope/")]
+        public Body Body { get; set; }
+        [XmlAttribute(AttributeName = "soap", Namespace = "http://www.w3.org/2000/xmlns/")]
+        public string Soap { get; set; }
+    }
+
+}
